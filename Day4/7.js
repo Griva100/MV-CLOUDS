57170fbb8dbf671f97f6b08f2fad08ba26d07afc
Griva100 <griva1009@gmail.com>
2024-01-08 14:25:01 +0530

    Day Task

--- Day4/7.js
+++
@@ -1,8 +0,0 @@
-// 7. Create a local text file that contains words in it. Using JavaScript read the words
-// from a text file and create a recursive function that identifies the very first item
-// that has recurred in the word. It returns the identified item with the index where it
-// first appeared and the very next index where it reappeared - entirely as an object.
-// Ex.first word from file - “BTEJHBERSD” ➞ {“E”: [2, 6]}
-//    2nd word from file - “JPDETRETXD” ➞ {“E”: [3, 6]}
-//    … and go on.
-
